ddba48c1f9d474c725647b8a22c35315344cc433
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-17 21:39:26 +0200

    Something

--- NoMoreGrind.cs
+++ NoMoreGrind.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 using Upgradeables;
+
+using System.Reflection;
 
 namespace NoMoreGrind
 {
@@ -19,8 +22,23 @@ namespace NoMoreGrind
 
         public void Start()
         {
+            List<FieldInfo> fields = new List<FieldInfo>
+            (
+                typeof(UpgradeableFacility).GetFields
+                (
+                    BindingFlags.NonPublic | BindingFlags.Instance
+                )
+            );
+
+            var levelField =
+                    new List<FieldInfo>(
+                        fields.Where<FieldInfo>(
+                            f => f.FieldType.Equals(typeof(UpgradeableObject.UpgradeLevel)))).First();
+
             foreach (UpgradeableFacility facility in GameObject.FindObjectsOfType<UpgradeableFacility>())
             {
+                UpgradeableObject.UpgradeLevel level = (UpgradeableObject.UpgradeLevel)levelField.GetValue(facility);
+                level.levelCost *= 0.1f;
             }
         }
          
