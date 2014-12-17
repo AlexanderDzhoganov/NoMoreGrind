c2ef93850cb84cf7ff21c8df13e409d6d460e4e9
ddaf <ddfreiling@gmail.com>
2014-12-17 21:29:13 +0100

    fixed upgradelevel retrieval

--- NoMoreGrind.cs
+++ NoMoreGrind.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using UnityEngine;
 using Upgradeables;
+using UpgradeLevel = Upgradeables.UpgradeableObject.UpgradeLevel;
 
 using System.Reflection;
 
@@ -13,8 +14,8 @@ namespace NoMoreGrind
     [KSPAddon(KSPAddon.Startup.SpaceCentre, true)]
     public class NoMoreGrind : MonoBehaviour
     {
-      
-       
+        private static float CostMultiplier = 0.1f;
+
         public void Awake()
         {
             print("NoMoreGrind: Initialized");
@@ -29,17 +30,29 @@ namespace NoMoreGrind
                     BindingFlags.NonPublic | BindingFlags.Instance
                 )
             );
+            Debug.Log("Found " + fields.Count + " priv fields in UpgradableFacility");
 
-            var levelField =
-                    new List<FieldInfo>(
+            List<FieldInfo> upgradeLevelsFields =
+                    (new List<FieldInfo>(
                         fields.Where<FieldInfo>(
-                            f => f.FieldType.Equals(typeof(UpgradeableObject.UpgradeLevel)))).First();
+                            f => f.FieldType.Equals(typeof(UpgradeLevel[])))));
+            Debug.Log("Found " + upgradeLevelsFields.Count + " UpgradeLevels fields");
 
+            Debug.LogError("### NoMoreGrind ###");
             foreach (UpgradeableFacility facility in GameObject.FindObjectsOfType<UpgradeableFacility>())
             {
-                UpgradeableObject.UpgradeLevel level = (UpgradeableObject.UpgradeLevel)levelField.GetValue(facility);
-                level.levelCost *= 0.1f;
+                Debug.LogWarning(facility.name + " has upgrade-cose: " + facility.GetUpgradeCost());
+
+                UpgradeLevel[] upgradeLevels = (UpgradeLevel[])upgradeLevelsFields[0].GetValue(facility);
+                for (int i = 0; i < upgradeLevels.Length; i++)
+                {
+                    UpgradeLevel level = upgradeLevels[i];
+                    Debug.Log("Level " + i + " costs " + level.levelCost);
+                    level.levelCost *= CostMultiplier;
+                    Debug.Log("> set to: " + level.levelCost);
+                }
             }
+            Debug.LogError("### NoMoreGrind ###");
         }
 
         public void LoadState(ConfigNode configNode)
