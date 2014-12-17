0dcb0c32e967c336016f72eb8d483fb19f0d7779
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-17 22:51:12 +0200

    merged gui with main

--- NoMoreGrind.cs
+++ NoMoreGrind.cs
@@ -16,6 +16,10 @@ namespace NoMoreGrind
     {
         private static float CostMultiplier = 0.1f;
 
+        private Rect windowRect = new Rect(256, 256, 320, 240);
+
+        private bool isVisible = false;
+
         public void Awake()
         {
             print("NoMoreGrind: Initialized");
@@ -30,15 +34,12 @@ namespace NoMoreGrind
                     BindingFlags.NonPublic | BindingFlags.Instance
                 )
             );
-            Debug.Log("Found " + fields.Count + " priv fields in UpgradableFacility");
 
             List<FieldInfo> upgradeLevelsFields =
                     (new List<FieldInfo>(
                         fields.Where<FieldInfo>(
                             f => f.FieldType.Equals(typeof(UpgradeLevel[])))));
-            Debug.Log("Found " + upgradeLevelsFields.Count + " UpgradeLevels fields");
 
-            Debug.LogError("### NoMoreGrind ###");
             foreach (UpgradeableFacility facility in GameObject.FindObjectsOfType<UpgradeableFacility>())
             {
                 Debug.LogWarning(facility.name + " has upgrade-cose: " + facility.GetUpgradeCost());
@@ -47,34 +48,76 @@ namespace NoMoreGrind
                 for (int i = 0; i < upgradeLevels.Length; i++)
                 {
                     UpgradeLevel level = upgradeLevels[i];
-                    Debug.Log("Level " + i + " costs " + level.levelCost);
                     level.levelCost *= CostMultiplier;
-                    Debug.Log("> set to: " + level.levelCost);
                 }
             }
-            Debug.LogError("### NoMoreGrind ###");
         }
 
         public void LoadState(ConfigNode configNode)
         {
-            gui.LoadConfig(configNode);
+            if (configNode.HasValue("CostFactor"))
+            {
+                try
+                {
+                    CostMultiplier = float.Parse(configNode.GetValue("CostFactor"));
+                }
+                catch (Exception)
+                {
+                    CostMultiplier = 0.1f;
+                }   
+            }
         }
 
         public void SaveState(ConfigNode configNode)
         {
-            gui.SaveConfig(configNode);    
+            if (configNode.HasValue("CostFactor"))
+            {
+                configNode.SetValue("CostFactor", CostMultiplier.ToString());
+            }
+            else
+            {
+                configNode.AddValue("CostFactor", CostMultiplier.ToString());
+            }
         }
 
         void OnGUI()
         {
-            gui.OnGUI();
-        }
+            if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.F11))
+            {
+                isVisible = true;
+            }
 
-        private GUI gui = new GUI();
+            if (isVisible)
+            {
+                windowRect = GUILayout.Window(0, windowRect, DoWindow, "No More Grind");
+            }
+        }
 
         public void OnDestroy()
         {
-            print("NoMoreGrind: Deinitialized");
+        }
+
+        private void DoWindow(int index)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("X", GUILayout.Width(16)))
+            {
+                isVisible = false;
+            }
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label("Cost factor");
+            GUILayout.FlexibleSpace();
+
+            CostMultiplier = GUILayout.HorizontalSlider(CostMultiplier, 0.05f, 2.0f);
+
+            GUILayout.Label(CostMultiplier.ToString("0.00"));
+            GUILayout.EndHorizontal();
         }
 
     }
