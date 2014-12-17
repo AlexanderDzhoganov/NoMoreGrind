7afe4b8837e826a1e753b2a0876370261ab58f8b
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-17 21:21:37 +0200

    Initial commit

---
+++ NoMoreGrind.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Upgradeables;
+
+namespace KSPAdvancedFlyByWire
+{
+
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, true)]
+    public class NoMoreGrind : MonoBehaviour
+    {
+      
+       
+        public void Awake()
+        {
+            print("NoMoreGrind: Initialized");
+        }
+
+        public void Start()
+        {
+            foreach (UpgradeableFacility facility in GameObject.FindObjectsOfType<UpgradeableFacility>())
+            {
+            }
+        }
+         
+        public void OnDestroy()
+        {
+            print("NoMoreGrind: Deinitialized");
+        }
+
+    }
+
+}
